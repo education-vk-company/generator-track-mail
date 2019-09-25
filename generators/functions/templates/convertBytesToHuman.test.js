990480cae66846139103662489b3d47aff1fc545
Martin Komitsky <m.komitsky@corp.mail.ru>
2019-09-25 06:31:59 +0300

    master: fixed test description

--- generators/functions/templates/convertBytesToHuman.test.js
+++ generators/functions/templates/convertBytesToHuman.test.js
@@ -2,9 +2,9 @@
  * Необходимо покрыть все возможные
  * и невозможные кейсы. Например,
  * convertBytesToHuman(-1) === false,
- * convertBytesToHuman(-1) !== 1,
+ * convertBytesToHuman(-1) !== '1 B',
  * convertBytesToHuman('string') === false
- * convertBytesToHuman(5) === 5
+ * convertBytesToHuman(5) === '5 B'
  */
 
 
